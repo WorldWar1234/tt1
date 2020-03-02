b2bb4bb29155854491ac1eb6b2c4852a11bed08f
Jason Sherman <jsn.sherman@gmail.com>
2020-03-01 21:30:16 -0600

    significantly reduce default quality.

--- src/params.js
+++ src/params.js
@@ -1,4 +1,4 @@
-const DEFAULT_QUALITY = 40;
+const DEFAULT_QUALITY = 2;
 
 function params(req, res, next) {
     let url = req.query.url;
@@ -8,7 +8,7 @@ function params(req, res, next) {
     url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
     req.params.url = url;
     req.params.webp = !req.query.jpeg;
-    req.params.grayscale = req.query.bw != 0;
+    req.params.grayscale = req.query.bw !== 0;
     req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY;
 
     next()
